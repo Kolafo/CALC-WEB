b52bd0a668c5318d833343e55d9d5ae6891f610a
Kolafo <kolafo@bk.ru>
2024-04-07 22:57:31 +1000

    нормас так

--- FRONT/calc-back.js
+++ FRONT/calc-back.js
@@ -1,3 +1,10 @@
+let num = ""
 function updateOutput(value) {
     document.getElementById('result').textContent += value;
+    num += value
+    console.log(num)
+}
+
+function backspace() {
+    document.getElementById('result').textContent = document.getElementById('result').textContent.slice(0, -1);
 }
