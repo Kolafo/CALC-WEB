428fbbee40fecf28423e1582c201f07f0bc20823
Kolafo <kolafo@bk.ru>
2024-04-07 22:50:57 +1000

    нормас так

--- FRONT/calc-back.js
+++ FRONT/calc-back.js
@@ -1,13 +1,3 @@
-let num = "";
-let ans = [];
 function updateOutput(value) {
     document.getElementById('result').textContent += value;
-    ans += value
-}
-function operate(value) {
-    document.getElementById('operator').textContent = value;
-}
-function clearOutput() {
-    document.getElementById('result').textContent = '';
-    ans = []
 }
