fad2d249bf9ca22e9a9047272e9fa1b952894d39
Kolafo <kolafo@bk.ru>
2024-04-08 00:05:09 +1000

    нормас так

--- FRONT/calc-back.js
+++ FRONT/calc-back.js
@@ -1,10 +1,21 @@
 let num = ""
+let operato = ""
 function updateOutput(value) {
+    if (value=',' and num.inclued(',')) return{
+
     document.getElementById('result').textContent += value;
-    num += value
-    console.log(num)
+    num += value;
+    console.log(num);
 }
 
 function backspace() {
     document.getElementById('result').textContent = document.getElementById('result').textContent.slice(0, -1);
+    num = num.slice(0, -1);
+    console.log(num);
+}
+
+function operator(value) {
+    document.getElementById('result').textContent += value;
+    operato = value;
+    console.log(operato);
 }
