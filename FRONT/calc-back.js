383419f483abaf9a4b43fec5122ee5523039eb5a
Kolafo <kolafo@bk.ru>
2024-04-09 01:15:14 +1000

    work

--- FRONT/calc-back.js
+++ FRONT/calc-back.js
@@ -10,16 +10,23 @@ function clearAll() {
 }
 
 function backspace() {
+    if (document.getElementById('result').innerText === '0') {
+        return;
+    }
     if (current.length === 1 || current==='') {
-        current = '0';
+        document.getElementById('result').innerText = '0';
+        current = '';
     } else {
         current = current.slice(0, -1);
+        document.getElementById('result').innerText = current;
     }
-    document.getElementById('result').innerText = current;
 }
 
 function insert(value) {
-    if (value==='0' && document.getElementById('result').innerText==='0') {
+    if ((value==='0' || value==='.') && document.getElementById('result').innerText==='0') {
+        return;
+    }
+    if (value==='.' && current.includes('.')) {
         return;
     }
     current += value;
@@ -28,8 +35,23 @@ function insert(value) {
 
 function operando(value) {
     if (current.length != 0) {
+
         all = current + value
         current = '';
         document.getElementById('result').innerText = all;
     }
 }
+
+const container = document.querySelector('.output');
+
+container.addEventListener('input', function() {
+    container.scrollLeft = container.scrollWidth;
+});
+const scrollText = () => {
+    if (isScrolling) {
+        container.scrollLeft = container.scrollWidth;
+        isScrolling = false;
+    }
+}
+
+setInterval(scrollText, 100); // Проверка каждые 100 миллисекунд
